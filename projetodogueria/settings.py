f2e047f8b675dfc89078876564b71c52f7849a88
Antonio <antoniohenriquedd15@hotmail.com>
2020-05-21 20:36:52 -0300

    Adicionando alguns arquivos para deploy

--- projetodogueria/settings.py
+++ projetodogueria/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&xvo21#y-(=(*-o(!lfo)=h=sw=+!f!=!a1^gj)d8n2-a$keni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
