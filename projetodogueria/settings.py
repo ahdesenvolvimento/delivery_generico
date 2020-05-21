b5d896d1d968a0da35699f488c338f4244a690bd
Antonio <antoniohenriquedd15@hotmail.com>
2020-05-21 20:39:26 -0300

    Adicionando arquivos estaticos

--- projetodogueria/settings.py
+++ projetodogueria/settings.py
@@ -129,3 +129,5 @@ LOGOUT_REDIRECT_URL = 'index'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
---
+++ staticfiles/js/funcoes_js.js
@@ -0,0 +1,7 @@
+$(function(e){
+
+    $('#modal_carrinho').on('show.bs.modal', function(e){
+        $('#modal_carrinho').fadeToggle(50);
+    });
+  
+});
